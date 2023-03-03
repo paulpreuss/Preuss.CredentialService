f73826ebfbb5d54ac97f2c32cc9332f479f93b14
paulpreuss <pauljonas.preuss@icloud.com>
2023-03-03 22:05:31 +0100

    filled interface for credential repository

--- Preuss.CredentialService.MongoAccess.Abstracts/Repositories/ICredentialRepository.cs
+++ Preuss.CredentialService.MongoAccess.Abstracts/Repositories/ICredentialRepository.cs
@@ -1,5 +1,12 @@
-﻿namespace Preuss.CredentialService.MongoAccess.Abstracts.Repositories;
+﻿using Preuss.CredentialService.MongoAccess.Abstracts.DataClasses;
+
+namespace Preuss.CredentialService.MongoAccess.Abstracts.Repositories;
 
 public interface ICredentialRepository
 {
+    Task<List<Credentials>> GetAllCredentialsAsync();
+    Task<Credentials> GetCredentialsByNameAsync(string name);
+    Task AddCredentials(Credentials credentials);
+    Task UpdateCredentials(Credentials credentials);
+    Task DeteleCredentials(Credentials credentials);
 }
