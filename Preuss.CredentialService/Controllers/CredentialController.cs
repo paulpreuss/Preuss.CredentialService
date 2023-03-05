976d6dce7baa51eae6fe9e941dbb8f31d0c16597
paulpreuss <pauljonas.preuss@icloud.com>
2023-03-05 12:02:18 +0100

    created routes for login and creation

--- Preuss.CredentialService/Controllers/CredentialController.cs
+++ Preuss.CredentialService/Controllers/CredentialController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Preuss.CredentialService.Abstracts.Processors;
 using Preuss.CredentialService.MongoAccess.Abstracts.DataClasses;
+using Preuss.CredentialService.Validation.Abstracts;
 
 namespace Preuss.CredentialService.Controllers;
 
@@ -11,20 +12,65 @@ public class CredentialController : ControllerBase
 {
     private readonly ILogger<CredentialController> _logger;
     private readonly ICredentialProcessor _proseccor;
+    private readonly ICredentialsValidator _validator;
 
     public CredentialController(ILogger<CredentialController> logger,
-        ICredentialProcessor processor)
+        ICredentialProcessor processor, ICredentialsValidator validator)
 	{
         _logger = logger;
         _proseccor = processor;
+        _validator = validator;
 	}
 
-    [HttpGet(Name = "Login")]
-    public async Task<IActionResult> Get()
+    [HttpPost(Name = "Login")]
+    [Route("[controller]/login")]
+    public async Task<IActionResult> Login([FromBody] Credentials credentials)
     {
-        var user = await _proseccor.LoginAsync("", "");
+        try
+        {
+            if (string.IsNullOrWhiteSpace(credentials.Username))
+                return new BadRequestObjectResult("User name is not valid.");
 
-        return new OkObjectResult(user);
+            if (string.IsNullOrWhiteSpace(credentials.HashedPassword))
+                return new BadRequestObjectResult("User password is not valid.");
+
+            var user = await _proseccor.LoginAsync(credentials.Username,
+                credentials.HashedPassword);
+
+            if (user is null)
+                return new BadRequestObjectResult("User not found.");
+
+            return new OkObjectResult(user);
+        }
+        catch (Exception ex)
+        {
+            return new BadRequestObjectResult(ex);
+        }
+    }
+
+    [HttpPost(Name = "CreateUser")]
+    [Route("[controller]/create-user")]
+    public async Task<IActionResult> CreateUser([FromBody] Credentials credentials)
+    {
+        try
+        {
+            if (!_validator.IsValidEmail(credentials.Email))
+                return new BadRequestObjectResult("User email is not valid.");
+
+            if (!_validator.IsValidUsername(credentials.Username))
+                return new BadRequestObjectResult("User name is not valid.");
+
+            if (!_validator.IsValidPassword(credentials.HashedPassword))
+                return new BadRequestObjectResult("User password is not valid.");
+
+            await _proseccor.CreateUserAsync(credentials, credentials.HashedPassword);
+
+            return new OkResult();
+        }
+        catch (Exception ex)
+        {
+            return new BadRequestObjectResult(ex);
+        }
     }
 }
 
